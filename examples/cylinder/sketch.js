83e73df2e9a3842ba1e31b1690f47deb10b51f19
nakednous <nakednous@gmail.com>
2022-02-17 10:42:40 -0500

    cylinder onscreen exmaple updated

--- examples/cylinder/sketch.js
+++ examples/cylinder/sketch.js
@@ -1,13 +1,24 @@
+let img;
+
+function preload() {
+  img = loadImage('img.jpg');
+}
+
 function setup() {
   createCanvas(400, 400, WEBGL);
   noStroke();
+  textureMode(NORMAL);
 }
 
 function draw() {
   background(200);
-  orbitControl();
+  //orbitControl();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
   fill(255, 0, 0);
-  cylinder({ radius: 50, detail : 10 });
+  texture(img);
+  cylinder({ radius: 50, detail : 32, texture: true});
 }
 
 function windowResized() {
