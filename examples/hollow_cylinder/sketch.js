49fbb11a7c686e46e76d49ae38374d9dea5002b8
nakednous <nakednous@gmail.com>
2022-02-19 15:05:37 -0500

    hollowCylinder examples greatly improved

--- examples/hollow_cylinder/sketch.js
+++ examples/hollow_cylinder/sketch.js
@@ -1,4 +1,9 @@
+// texture
 let img;
+// check box
+let auto_rotate;
+// select
+let mode;
 
 function preload() {
   img = loadImage('img.jpg');
@@ -6,21 +11,42 @@ function preload() {
 
 function setup() {
   createCanvas(400, 400, WEBGL);
-  noStroke();
   textureMode(NORMAL);
+  auto_rotate = createCheckbox('auto rotate', true);
+  auto_rotate.style('color', 'magenta');
+  auto_rotate.position(10, 10);
+  mode = createSelect();
+  mode.position(10, 40);
+  mode.option('Fill');
+  mode.option('Wiredframe');
+  mode.option('Texture');
+  mode.value('Wiredframe');
 }
 
 function draw() {
-  //orbitControl();
   background(200);
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  //fill(255, 0, 0);
-  //texture(img);
-  noFill();
-  stroke(0, 255, 255);
+  if (auto_rotate.checked()) {
+    rotateZ(frameCount * 0.01);
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.01);
+  }
+  else {
+    orbitControl();
+  }
+  push();
+  switch(mode.value()) {
+    case 'Fill':
+      fill(255, 0, 0);
+      break;
+    case 'Wiredframe':
+      noFill();
+      stroke(0, 255, 255);
+      break;
+    default:
+      texture(img);
+  }
   hollowCylinder({ radius: 50, detail : 32 });
+  pop();
 }
 
 function windowResized() {
--- examples/hollow_cylinder_off/sketch.js
+++ examples/hollow_cylinder_off/sketch.js
@@ -1,5 +1,11 @@
-let pg;
+// texture
 let img;
+// p5.Graphics
+let pg;
+// check boxes
+let hud;
+// select
+let mode;
 
 function preload() {
   img = loadImage('img.png');
@@ -9,13 +15,21 @@ function setup() {
   createCanvas(400, 400, WEBGL);
   pg = createGraphics(300, 300, WEBGL);
   pg.textureMode(NORMAL);
+  hud = createCheckbox('Heads Up Display', true);
+  hud.style('color', 'cyan');
+  hud.position(10, 10);
+  mode = createSelect();
+  mode.position(10, 40);
+  mode.option('Fill');
+  mode.option('Wiredframe');
+  mode.option('Texture');
+  mode.value('Texture');
 }
 
 function draw() {
   background('#316DCA');
-  //it seems orbit control isn't implemented for a given pg
-  orbitControl();
-  pg.background(0);  
+  pg.push();
+  pg.background(0);
   /*
   key line, since from here:
   https://p5js.org/reference/#/p5.Graphics
@@ -26,6 +40,7 @@ function draw() {
   copy the behavior of the standard canvas.
   */
   pg.reset();
+  /*
   pg.rotateZ(frameCount * 0.01);
   pg.rotateX(frameCount * 0.01);
   pg.rotateY(frameCount * 0.01);
@@ -34,7 +49,37 @@ function draw() {
   pg.noFill();
   pg.stroke(255, 0, 255);
   pg.hollowCylinder({ radius: 50, detail : 32 });
+  */
+  switch(mode.value()) {
+    case 'Fill':
+      pg.fill(255, 0, 0);
+      break;
+    case 'Wiredframe':
+      pg.noFill();
+      pg.stroke(0, 255, 255);
+      break;
+    default:
+      pg.texture(img);
+  }
+  pg.rotateZ(frameCount * 0.01);
+  pg.rotateX(frameCount * 0.01);
+  pg.rotateY(frameCount * 0.01);
+  pg.hollowCylinder({ radius: 50, detail : 32 });
+  if(hud.checked()) {
+    // init p5.treegl.HUD
+    beginHUD();
+  }
+  else {
+    rotateZ(frameCount * 0.01);
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.01);
+  }
   image(pg, -150, -150);
+  if(hud.checked()) {
+    // end p5.treegl.HUD
+    endHUD();
+  }
+  pg.pop();
 }
 
 function windowResized() {
