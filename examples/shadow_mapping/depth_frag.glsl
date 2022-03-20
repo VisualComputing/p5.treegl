67456cf83bb19db738773bbbc28212b2078928ef
nakednous <nakednous@gmail.com>
2022-03-20 17:53:13 -0500

    shadowmap init

---
+++ examples/shadow_mapping/depth_frag.glsl
@@ -0,0 +1,10 @@
+precision highp float;
+
+vec4 packDepth(float depth) {
+    float depthFrac = fract(depth * 255.0);
+    return vec4(depth - depthFrac / 255.0, depthFrac, 1.0, 1.0);
+}
+
+void main(void) {
+    gl_FragColor = packDepth(gl_FragCoord.z);
+}
