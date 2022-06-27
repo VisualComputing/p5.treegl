2c68eb7c3cd80a0d1965d4055a7b9040ceaa457e
nakednous <nakednous@gmail.com>
2022-06-26 19:01:21 -0500

    example improved

--- examples/picking/sketch.js
+++ examples/picking/sketch.js
@@ -1,15 +1,13 @@
 let easycam;
-let boxes;
+let models;
 let picked, squared, cached = true;
+let pv, e, m;
 
 function setup() {
-  //pixelDensity(1);
   createCanvas(400, 400, WEBGL);
-  //setAttributes('antialias', true);
-
   // define initial state
   let state = {
-    distance: 164.411,
+    distance: 300,
     center: [0, 0, 0],
     rotation: [-0.285, -0.257, -0.619, 0.685],
   };
@@ -19,12 +17,11 @@ function setup() {
   easycam.setState(state, 2000); // now animate to that state
   // suppress right-click context menu
   document.oncontextmenu = function () { return false; }
-
   colorMode(RGB, 1);
   let trange = 100;
-  boxes = [];
+  models = [];
   for (let i = 0; i < 25; i++) {
-    boxes.push(
+    models.push(
       {
         position: createVector((random() * 2 - 1) * trange, (random() * 2 - 1) * trange, (random() * 2 - 1) * trange),
         size: random() * 25 + 8,
@@ -33,8 +30,6 @@ function setup() {
     );
   }
 }
-
-let p, v, pv, e, mat;
 
 function draw() {
   // (optionally) cache pv and e matrices to speedup computations
@@ -45,26 +40,26 @@ function draw() {
   background(0.5);
   axes();
   grid();
-  boxes.forEach(element => {
+  models.forEach(element => {
     push();
     translate(element.position);
-    // cache model matrix, just before drawing it
-    mat = mMatrix();
+    // cache model rix, just before drawing it
+    m = mMatrix();
     // non-cache version (perhaps slower, but it really depends on the number of models to be processed)
-    let picked = cached ? mousePicking({ mMatrix: mat, size: element.size * 2.5, pvMatrix: pv, eMatrix: e, shape: squared ? Tree.SQUARE : Tree.CIRCLE })
-      : mousePicking({ mMatrix: mat, size: element.size * 2.5, shape: squared ? Tree.SQUARE : Tree.CIRCLE })
+    let picked = cached ? mousePicking({ mMatrix: m, size: element.size * 2.5, pvMatrix: pv, eMatrix: e, shape: squared ? Tree.SQUARE : Tree.CIRCLE })
+      : mousePicking({ mMatrix: m, size: element.size * 2.5, shape: squared ? Tree.SQUARE : Tree.CIRCLE })
     fill(picked ? 'white' : element.color);
     noStroke();
     squared ? box(element.size) : sphere(element.size);
     strokeWeight(3);
     stroke(picked ? 'yellow' : cached ? 'red' : 'blue');
     if (cached) {
-      //cross({ mMatrix: mat, size: element.size * 2.5, pvMatrix: pv, eMatrix: e, shape: squared ? Tree.SQUARE : Tree.CIRCLE });
-      bullsEye({ mMatrix: mat, size: element.size * 2.5, pvMatrix: pv, eMatrix: e, shape: squared ? Tree.SQUARE : Tree.CIRCLE });
+      //cross({ mMatrix: m, size: element.size * 2.5, pvMatrix: pv, eMatrix: e, shape: squared ? Tree.SQUARE : Tree.CIRCLE });
+      bullsEye({ mMatrix: m, size: element.size * 2.5, pvMatrix: pv, eMatrix: e, shape: squared ? Tree.SQUARE : Tree.CIRCLE });
     }
     else {
-      //cross({ mMatrix: mat, size: element.size * 2.5, shape: squared ? Tree.SQUARE : Tree.CIRCLE });
-      bullsEye({ mMatrix: mat, size: element.size * 2.5, shape: squared ? Tree.SQUARE : Tree.CIRCLE });
+      //cross({ mMatrix: m, size: element.size * 2.5, shape: squared ? Tree.SQUARE : Tree.CIRCLE });
+      bullsEye({ mMatrix: m, size: element.size * 2.5, shape: squared ? Tree.SQUARE : Tree.CIRCLE });
     }
     pop();
   }
